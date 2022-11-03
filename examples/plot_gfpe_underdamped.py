064327a2f1bd8e2c005939b8b877f3e247263c5e
Hadrien <hadrien.vroylandt@sorbonne-universite.fr>
2022-11-04 00:29:42 +0100

    2D animation

--- examples/plot_gfpe_underdamped.py
+++ examples/plot_gfpe_underdamped.py
@@ -11,7 +11,7 @@ How to run kernel estimation
 
 import numpy as np
 import matplotlib.pyplot as plt
-import xarray as xr
+import matplotlib.animation as animation
 
 import VolterraBasis as vb
 import VolterraBasis.basis as bf
@@ -49,5 +49,43 @@ axs.plot(mymem.time, mymem.kernel[:, basis_comb.comb_indices(0, 0), basis_comb.c
 axs.plot(mymem.time, mymem.kernel[:, basis_comb.comb_indices(1, 0), basis_comb.comb_indices(0, 0)], "-x")
 axs.plot(mymem.time, mymem.kernel[:, basis_comb.comb_indices(0, 0), basis_comb.comb_indices(1, 0)], "-x")
 axs.plot(mymem.time, mymem.kernel[:, basis_comb.comb_indices(1, 0), basis_comb.comb_indices(1, 0)], "-x")
+
+
+# Survival problem
+sink_index = basis_comb.comb_indices(1, 1)
+mymem.kernel[:, :, sink_index] = 0
+p0 = np.zeros(mymem.dim_obs)
+p0[0] = 1
+t_new, p_t = mymem.solve_gfpe(5000, method="trapz", p0=p0)
+p_t = p_t[:, :, 0]
+fig_pt = plt.figure("Probability of time")
+plt.grid()
+plt.plot(t_new, p_t[:, 0], "-")
+
+
+fig, ax_anim = plt.subplots()
+ax_anim.grid()
+dt = t_new[1] - t_new[0]
+time_text = ax_anim.text(0.85, 0.95, "0.0", horizontalalignment="left", verticalalignment="top", transform=ax_anim.transAxes)
+
+xrange = np.linspace(0.8, 3.0, 50)
+yrange = np.linspace(-2.0, 2.0, 50)
+# Do mesh
+xx, yy = np.meshgrid(xrange, yrange)
+E_eval = mymem.basis_vector(vb.pos_gle_base._convert_input_array_for_evaluation(np.column_stack((xx, yy)), 2), compute_for="force")
+proba_val = E_eval @ p_t[0, :]
+print(E_eval.shape, proba_val.shape)
+ln = ax_anim.pcolormesh(xx, yy, proba_val.reshape(50, 50).T, shading="gouraud")
+
+
+def update(frame):
+    proba_val = E_eval @ p_t[frame, :]
+    ln.set_array(proba_val.reshape(50, 50).T)
+    time_text.set_text("%.3f" % (frame * dt))
+    return (ln, time_text)
+
+
+ani = animation.FuncAnimation(fig, update, frames=np.arange(p_t.shape[0]), blit=True, interval=10)
+
 
 plt.show()
